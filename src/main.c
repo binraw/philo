7111dda8bca347e9e97227ea5b11c8bb5d2538dd
binraw <truvelotrobin@yahoo.fr>
2024-03-16 12:25:35 +0100

    fix

--- src/main.c
+++ src/main.c
@@ -18,3 +18,25 @@ void    init_values(data_t *philo, char **argv)
     else
         philo->number_of_times_each_philosopher_must_eat = 0;
 }
+
+
+void    *my_thread_to_sleep(data_t *philo, void *args)
+{
+    usleep(philo->time_to_sleep); // mettre une valeur
+    printf("Philosopher %d is sleeping\n", (int) args ); // mettre la position du philo
+    return (NULL);
+}
+
+void    *my_thread_to_eat(data_t *philo, void *args)
+{
+    usleep(philo->time_to_eat);
+    printf("Philosopher %d is eating\n", (int) args );
+    return (NULL);
+}
+
+void    *my_thread_to_think(data_t *philo, void *args)
+{
+    usleep(50);
+    printf("Philosopher %d is thinking\n", (int) args);
+    return (NULL);
+}
